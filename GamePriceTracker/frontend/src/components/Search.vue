fba1c869b4273c25bfc2af22a14d51a1249e09da
Ella Rieder <65261267+riedee@users.noreply.github.com>
2022-02-11 15:21:05 -0500

    Redo

--- GamePriceTracker/frontend/src/components/Search.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div @submit.prevent="submitSearch">
-        <input type="text" v-model="game" placeholder="Search game">
-        <button type="submit">Submit</button>
-    </div>
-</template>
--- GamePriceTracker/frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Search from './components/Search.vue'
-
-createApp(Search).mount('#app')
