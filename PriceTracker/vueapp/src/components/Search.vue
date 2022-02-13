185c25a23e01e34de6bc880b22bd1e71f1231400
Ella Rieder <ellagracerieder@yahoo.com>
2022-02-13 16:29:00 -0500

    Update

---
+++ PriceTracker/vueapp/src/components/Search.vue
@@ -0,0 +1,6 @@
+<template>
+    <div @submit.prevent="submitSearch">
+        <input type="text" v-model="game" placeholder="Search game">
+        <button type="submit">Submit</button>
+    </div>
+</template>
