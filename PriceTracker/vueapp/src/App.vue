e8b0f28a3ff5f408c83bb2b01cf67fbe5fc48e5f
Jennifer Richardson <jarich150@gmail.com>
2022-02-14 16:21:39 -0500

    basic api call
    uses axios to call a website and get a response

--- PriceTracker/vueapp/src/App.vue
+++ PriceTracker/vueapp/src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+          "https://www.google.com/search?q=video+games&rlz=1C1CHBF_enUS864US864&oq=video+games&aqs=chrome.0.0i433i512l2j0i131i433i512j0i433i512j46i512j0i512j0i457i512j0i402l2j0i131i433i512.2867j0j7&sourceid=chrome&ie=UTF-8"
+        );
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    created() {
+      this.getData();
+    }
+  },
+};
 </script>
 
 <style>
