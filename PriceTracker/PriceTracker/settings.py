bbdd3a270911845fe732c07db85469bcc6cc35b9
Tinashe Osewe <t.osewe1@gmail.com>
2022-02-14 17:16:52 -0500

    added secret key

--- PriceTracker/PriceTracker/settings.py
+++ PriceTracker/PriceTracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--v-i7+1_$@*s+z0n*)mbc@@qb_yk+rpqo-@blnrap*4_=$s9=i'
+# SECRET_KEY = 'django-insecure--v-i7+1_$@*s+z0n*)mbc@@qb_yk+rpqo-@blnrap*4_=$s9=i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
