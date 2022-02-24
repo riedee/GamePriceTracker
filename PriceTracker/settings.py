84a459aef4a3a51a8c77f9fa5e442144e112de23
Jennifer Richardson <jarich150@gmail.com>
2022-02-24 12:02:12 -0500

    changes for lightsail deployment

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.89.199.232']
 
 
 # Application definition
--- PriceTrackerApp/views.py
+++ PriceTrackerApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def vue_test(request):
     return render(request, 'PriceTrackerApp/vue-test.html')
+
+def index(request):
+    return HttpResponse("Welcome to Game Price Tracker!")
