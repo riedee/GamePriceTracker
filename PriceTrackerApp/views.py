9063784936df18a60b20275f157f2b992aadf958
Jennifer Richardson <jarich150@gmail.com>
2022-02-28 23:29:53 -0500

    delete comment

--- PriceTrackerApp/views.py
+++ PriceTrackerApp/views.py
@@ -4,10 +4,6 @@ from django.http import HttpResponse
 #models
 from .models import *
 
-# Create your views here.
-#def vue_test(request):
-    #return render(request, 'PriceTrackerApp/vue-test.html')
-
 def index(request):
     return HttpResponse("Welcome to Game Price Tracker!")
     
