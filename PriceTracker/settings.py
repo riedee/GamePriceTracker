68f0c99b1cb2c0550a61b126b9746b188c4a3c46
mark <markv_99@hotmail.com>
2022-03-01 14:41:14 -0500

    edit static settings

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -117,9 +117,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = 'var/static_root/'
+#STATICFILES_DIRS = ['static']
+
 STATIC_URL = '/static/'
-STATIC_ROOT = 'var/static_root/'
-STATICFILES_DIRS = ['static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+	
+STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static/'),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+# Default 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
