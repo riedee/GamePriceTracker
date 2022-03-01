38a60168c836621c3643fd7978efc8f14a65b4de
mark <markv_99@hotmail.com>
2022-03-01 09:14:22 -0500

    search view

--- PriceTrackerApp/views.py
+++ PriceTrackerApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+
 
 #models
 from .models import *
@@ -12,3 +14,18 @@ def search(request):
 	context = {
                         }
 	return render(request, 'PriceTrackerApp/search.html', context)
+
+class SearchView(ListView):
+    model = Game
+    template_name = 'search_results.html'
+    context_object_name = 'all_search_results'
+
+    def get_queryset(self):
+       result = super(SearchView, self).get_queryset()
+       query = self.request.GET.get('search')
+       if query:
+          postresult = Game.objects.filter(gameTitle__contains=query)
+          result = postresult
+       else:
+           result = None
+       return result
