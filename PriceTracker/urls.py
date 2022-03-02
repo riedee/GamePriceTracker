63b522abffc8d80d5b1cd21965858d4751f98b06
Ella Rieder <ellagracerieder@yahoo.com>
2022-03-01 20:04:02 -0500

    Update homepage with logo

--- PriceTracker/urls.py
+++ PriceTracker/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from PriceTrackerApp import views as app_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('PriceTrackerApp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
