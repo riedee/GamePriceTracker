35101ee474969d8bc90502bb2a1b0c1a6502a262
Jennifer Richardson <jarich150@gmail.com>
2022-03-03 11:10:47 -0500

    update requirements and scraper

--- PriceTrackerApp/scraper.py
+++ PriceTrackerApp/scraper.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup
+import bs4
 from urllib.request import urlopen
 from .models import *
 
 url = "https://www.amazon.com/Pokemon-Legends-Arceus-Nintendo-Switch/dp/B09BNK4592/ref=sr_1_1_sspa?keywords=pokemon%2Blegends%2Barceus%2Bnintendo%2Bswitch&qid=1646319774&sprefix=pokemon%2B%2Caps%2C66&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFYUUo0WDJMWUpZUlcmZW5jcnlwdGVkSWQ9QTAwMDU0NDIzTkVSUDhVSENUQ0dFJmVuY3J5cHRlZEFkSWQ9QTA4Njk2NTczVThSMEpLR1E1REJWJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1"
 page = urlopen(url)
 html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+soup = bs4.BeautifulSoup(html, "html.parser")
 
 parsed_url = urllib.parse.urlparse(url)
 vendorHost = parsed_url.netloc.string.strip()
