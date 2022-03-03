d111cd52cfa5954d9835c0be8e1f1236791f881d
Jennifer Richardson <jarich150@gmail.com>
2022-03-03 13:20:56 -0500

    scrape multiple pages

--- PriceTrackerApp/scraper.py
+++ PriceTrackerApp/scraper.py
@@ -2,14 +2,6 @@ import bs4
 from urllib.request import urlopen
 from .models import *
 
-url = "https://www.amazon.com/Pokemon-Legends-Arceus-Nintendo-Switch/dp/B09BNK4592/ref=sr_1_1_sspa?keywords=pokemon%2Blegends%2Barceus%2Bnintendo%2Bswitch&qid=1646319774&sprefix=pokemon%2B%2Caps%2C66&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFYUUo0WDJMWUpZUlcmZW5jcnlwdGVkSWQ9QTAwMDU0NDIzTkVSUDhVSENUQ0dFJmVuY3J5cHRlZEFkSWQ9QTA4Njk2NTczVThSMEpLR1E1REJWJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = bs4.BeautifulSoup(html, "html.parser")
-
-parsed_url = urllib.parse.urlparse(url)
-vendorHost = parsed_url.netloc.string.strip()
-
 def get_title(soup): 
     try:
         # Outer Tag Object
@@ -45,11 +37,20 @@ def get_platform(soup):
  
     return platform   
 
-title = get_title(soup)
-price = get_price(soup)
-platform = get_platform(soup)
-gameID = title + platform
-vendor = Vendor(vendorName = vendorHost)
-vendor.save()
-game = Game(gameTitle = title, bestVendor = vendor, lowestPrice = price, url = url, platform = platform, gameID = gameID)
-game.save()
+urlList = ["https://www.amazon.com/Pokemon-Legends-Arceus-Nintendo-Switch/dp/B0914YGQSH/ref=sr_1_1_sspa?keywords=pokemon%2Blegends%2Barceus%2Bnintendo%2Bswitch&qid=1646319774&sprefix=pokemon%2B%2Caps%2C66&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFYUUo0WDJMWUpZUlcmZW5jcnlwdGVkSWQ9QTAwMDU0NDIzTkVSUDhVSENUQ0dFJmVuY3J5cHRlZEFkSWQ9QTA4Njk2NTczVThSMEpLR1E1REJWJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1", "https://www.amazon.com/Just-Dance-2022-Nintendo-Switch/dp/B0973JSZMH/ref=sr_1_4?c=ts&keywords=Nintendo%2BSwitch%2BGames&qid=1646331513&s=videogames&sr=1-4&ts_id=16227133011&th=1", "https://www.amazon.com/Elden-Ring-Xbox-One/dp/B07SMBNTSJ/ref=sr_1_4?brr=1&pf_rd_p=63852bcf-d9b2-407d-acbe-b455e5d8f28a&pf_rd_r=BFT3BH4A0FG88S0FRFJE&pf_rd_s=videogames-subnav-flyout-content-15&pf_rd_t=SubnavFlyout&qid=1646331561&rd=1&s=videogames&sr=1-4&th=1"]
+
+for i in range(len(urlList)):
+    url = urlList[i]
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    parsed_url = urllib.parse.urlparse(url)
+    vendorHost = parsed_url.netloc.string.strip()
+    title = get_title(soup)
+    price = get_price(soup)
+    platform = get_platform(soup)
+    gameID = title + platform
+    vendor = Vendor(vendorName = vendorHost)
+    vendor.save()
+    game = Game(gameTitle = title, bestVendor = vendor, lowestPrice = price, url = url, platform = platform, gameID = gameID)
+    game.save()
