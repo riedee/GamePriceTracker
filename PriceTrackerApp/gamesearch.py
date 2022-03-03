963f0aaf97bee315b6fff8ff15dae122f672ab27
Ella Rieder <riedee@rpi.edu>
2022-03-03 16:55:33 -0500

    add method to find vendor links

---
+++ PriceTrackerApp/gamesearch.py
@@ -0,0 +1,16 @@
+from googlesearch import search
+
+#query should include 'buy' at the end to bring up most accurate results
+def searchGame(query):
+    links = []
+    for i in search(query, tld="co.in", num=10, stop=10, pause=3):
+        site = i.split(".")
+
+        #list of vendors, replace when we have list of specific vendors we're looking at
+        vendors = ["steampowered", "xbox", "playstation", "gamestop", "bestbuy", "nintendo", "amazon", "target", "walmart"]
+        for j in vendors:
+            if site[1] == j:
+                links.append(i)
+                break
+
+    return links
