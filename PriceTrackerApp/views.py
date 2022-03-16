9cf2892c6ae297c4e03a88c6c64fc1415568a244
Ella Rieder <ellagracerieder@yahoo.com>
2022-03-16 13:17:47 -0400

    resolve conflict

--- PriceTrackerApp/views.py
+++ PriceTrackerApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
+import json
 from PriceTrackerApp import gamesearch
 
 #models
