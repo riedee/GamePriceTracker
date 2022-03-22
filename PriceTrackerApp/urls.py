c365d2221ca83c7c05e6d193b75c330e5b64360e
Ella Rieder <ellagracerieder@yahoo.com>
2022-03-22 10:23:45 -0400

    update

--- PriceTrackerApp/urls.py
+++ PriceTrackerApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import HomeView, SearchResultsView, GameView, VendorPageView, VendorView, ProfileView, AmazonView
-from django.conf.urls import include, url
-from django.urls import re_path as url
+from django.conf.urls import include
+from django.urls import re_path
 from django.contrib import admin
 admin.autodiscover()
 
