987cd6cbca99f66590d1b366ba38e6b495b8f7d2
Jennifer Richardson <jarich150@gmail.com>
2022-03-28 10:49:39 -0400

    modify adduser.py to save the users to a JSON

--- PriceTrackerApp/adduser.py
+++ PriceTrackerApp/adduser.py
@@ -2,7 +2,9 @@
 from userapp.models import *
 from django.contrib.auth.models import User
 import csv
+import json
 #make sure file name is users.csv or change line below to match filename
+jsonFile = open('users.json', 'w')
 with open('users.csv','rt') as f:
     data = csv.reader(f)
     for row in data:
@@ -13,3 +15,5 @@ with open('users.csv','rt') as f:
         user.last_name = row[3]
         user.email = row[4]
         user.save()
+        userDict = {'username': row[0], 'password': row[1], 'fn': row[2], 'ln': row[3], 'email': row[4]}
+        json.dump(userDict, jsonFile)
