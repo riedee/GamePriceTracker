4b9308e70a163825a3607cd052f04a8a4507fd26
Tinashe Osewe <t.osewe1@gmail.com>
2022-03-29 21:00:27 -0400

    added custom authentication to main code

--- PriceTrackerApp/urls.py
+++ PriceTrackerApp/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from .views import HomeView, SearchResultsView, GameView, VendorPageView, VendorView, ProfileView, AmazonView, PlayStationView, NintendoView, MicrosoftView, SteamView, LoginView
+from django.urls import include, path
+from .views import HomeView, SearchResultsView, GameView, VendorPageView, VendorView, ProfileView, RegisterView, AmazonView, PlayStationView, NintendoView, MicrosoftView, SteamView, LoginView
 from django.conf.urls import include
 from django.urls import re_path
 from django.contrib import admin
@@ -8,9 +8,10 @@ admin.autodiscover()
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     path('search/', SearchResultsView, name='search_results'),
+    path('register/', RegisterView, name='register'),
     path('results/<str:info>', GameView, name='game'),
     path('login', LoginView.as_view(), name='login'),
-    path('home', HomeView.as_view(), name='home'),
+    path('home', HomeView, name='home'),
     path('vendors', VendorPageView.as_view(), name='vendorpage'),
     path('<int:user_id>/profile/', ProfileView, name='profile'),
     path('vendors/amazon/', AmazonView.as_view(), name = 'amazon'),
@@ -18,6 +19,7 @@ urlpatterns = [
     path('vendors/nintendo/', NintendoView.as_view(), name = 'nintendo'),
     path('vendors/microsoft/', MicrosoftView.as_view(), name = 'microsoft'),
     path('vendors/steam/', SteamView.as_view(), name = 'steam'),
+    path('accounts/', include('django.contrib.auth.urls')),
     
     #path('userlist', UserlistView, name='userlist'),
 
--- PriceTrackerApp/views.py
+++ PriceTrackerApp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from PriceTrackerApp.forms import RegistrationForm
 import json
 import os
 from PriceTrackerApp import gamesearch
@@ -15,8 +18,10 @@ with open(os.path.dirname(__file__) + '/../games.json', 'r') as f:
 def isSubstring(value, substring):
     return substring.lower() in value.lower()
 
-class HomeView(TemplateView):
-	template_name = 'home.html'
+def HomeView(request):
+    print(request.user.username)
+    username = request.user.username if request.user.is_authenticated else ''
+    return render(request, 'home.html', {'username' : username})
 	
 class VendorPageView(TemplateView):
 	template_name = 'vendorpage.html'
@@ -108,3 +113,19 @@ def ProfileView(request, user_id):
             'assigned': uid,
             }
     return render(request, "PriceTrackerApp/profile.html", context)
+
+def RegisterView(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
